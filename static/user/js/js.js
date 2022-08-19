ad28766ca2d8d5113326d781fbc4e054cc5646eb
dazarh <dazarh21@gmail.com>
2022-08-19 19:38:26 +0800

    add reqirements.txt

--- static/user/js/js.js
+++ static/user/js/js.js
@@ -1,2 +1,14 @@
-let font = document.querySelectorAll('.nav-element div')
-console.log('jjj')
+
+function makeFont() {
+    let screenWidth = window.innerWidth
+    let fonts = document.querySelectorAll('.nav-element-text');
+    if (screenWidth < 500) {
+        fonts.forEach((item)=> item.style.fontSize = '.8em')
+    } else if(screenWidth<800) {
+        fonts.forEach((item)=> item.style.fontSize = '1em')
+    } else
+        fonts.forEach((item)=> item.style.fontSize = '1.3em')
+    }
+
+
+makeFont()
